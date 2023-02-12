47b50672499b0d5f908af53ff48fab0068636328
Haris Nazir <haris.nazir1963@gmail.com>
2023-02-12 20:34:41 +0000

    Set up react app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Create a blank page with a form in the middle*/}
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
